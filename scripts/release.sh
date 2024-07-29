cc8868206e24f84850a4c3b0cd6db3d022aa511d
915640850@qq.com <915640850@qq.com>
2024-07-30 00:50:54 +0800

    fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,10 +3,13 @@
 # 本地路径
 DIST=dist
 # 服务器地址
-SERVER_HOST=119.29.19.xxx
+SERVER_HOST=45.76.204.22
 # 服务器路径
-TARGET_DIST=/www/wwwroot/
+TARGET_DIST=/www/wwwroot/shtest
 
 # 打包完，上传到服务器
 npm run build && \
 scp -r ./$DIST/* root@$SERVER_HOST:$TARGET_DIST
+
+# 服务器重启nginx
+ssh root@$SERVER_HOST "nginx -s reload"
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,7 +1,6 @@
 import { Buffer } from 'buffer';
 
 window.global = window.global ?? window;
-window.Buffer = window.Buffer ?? Buffer;
 window.process = window.process ?? { env: {} }; // Minimal process polyfill
 
 export {};
