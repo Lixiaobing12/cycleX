ec2f20bd48a7552efbc787ef2813c5daf7f8c9b9
915640850@qq.com <915640850@qq.com>
2024-07-31 00:17:52 +0800

    fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,11 +5,12 @@ DIST=dist
 # 服务器地址
 SERVER_HOST=45.76.204.22
 # 服务器路径
-TARGET_DIST=/www/wwwroot/shtest
+TARGET_DIST=/www/wwwroot/cyclex.monster-island.top
 
 # 打包完，上传到服务器
 npm run build && \
-scp -r ./$DIST/* root@$SERVER_HOST:$TARGET_DIST
+sshpass -p "#8vHw_!EDE7Jk[S=" scp -r ./$DIST/* root@$SERVER_HOST:$TARGET_DIST
 
 # 服务器重启nginx
-ssh root@$SERVER_HOST "nginx -s reload"
+sshpass -p "#8vHw_!EDE7Jk[S=" ssh root@$SERVER_HOST "nginx -s reload"
+
