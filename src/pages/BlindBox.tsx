b325fc359edddd0c0ff02dd1b96ed7c91e132494
lixiaobing12 <915640850@qq.com>
2024-08-11 23:14:17 +0800

    fix

--- src/pages/BlindBox.tsx
+++ src/pages/BlindBox.tsx
@@ -179,7 +179,7 @@ const BlindBox = () => {
   });
   const [data, setData] = useState([
     { title: "Sign Up", content: "Users are given 1 lottery chance for completing registration.", avatar: "/assets/blindbox-tasks-1.png", done: true, times: 1 },
-    { title: "Invitation", content: "Successful invitation rewards 1 time", avatar: "/assets/blindbox-tasks-2.png", done: false, times: 1 },
+    { title: "Invite Friends", content: "Successful invitation rewards 1 time", avatar: "/assets/blindbox-tasks-2.png", done: false, times: 1 },
     { title: "First Recharge", content: "First recharge rewards 2 times", avatar: "/assets/blindbox-tasks-4.png", done: false, times: 2 },
     { title: "Invest In Products", content: "Invest more than $10 and unclock more times", avatar: "/assets/blindbox-tasks-3.png", done: false, times: 5 },
   ]);
